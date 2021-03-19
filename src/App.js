735892becbe0ba4435ec7b22f6fc3d21b3c65174
Md. Akash Hossain <akash.cse.ruet@gmail.com>
2021-03-19 09:59:29 +0600

    login page updated

--- src/App.js
+++ src/App.js
@@ -1,37 +1,77 @@
 import './App.css';
-import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, useHistory } from "react-router-dom";
 import Home from './Components/Home/Home';
 import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 import ChooseRoute from './Components/ChooseRoute/ChooseRoute';
 import LoginPage from './Components/LoginPage/LoginPage';
 import { createContext } from 'react';
 import { useState } from 'react';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import navImg from "./images/Group 33141.png"
 
 export const UserContext = createContext();
 
 function App() {
   const [user, setUser] = useState({});
-  console.log("app ",user);
+  let history = useHistory();
+  console.log("app ", user);
+  const handleLogOut = () => {
+    setUser({});
+    
+  }
+  const logOut = ()=>{
+    history.push('/')
+  }
+  
+  
+
   return (
     <UserContext.Provider value={[user, setUser]}>
-      <h4>name: {user.displayName}</h4>
-      
-    <Router>
-      <Switch>
-        <Route exact path = "/">
-          <Home></Home>
-        </Route>
-        <Route path = "/home">
-          <Home></Home>
-        </Route>
-        <Route path = "/login">
-          <LoginPage></LoginPage>
-        </Route>
-        <PrivateRoute path="/route">
-          <ChooseRoute></ChooseRoute>
-        </PrivateRoute>
-      </Switch>
-    </Router>
+
+      <Router>
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand as={Link} to="/home"><img
+            src={navImg}
+            width="60"
+            height="50"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          /></Navbar.Brand>
+          <Nav className="mr-auto text-left">
+            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link href="#features">Destination</Nav.Link>
+            <Nav.Link href="#pricing">Blog</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+            <Navbar.Brand> <img src={user.photoURL}
+              width="60"
+              height="50"
+              style={{ borderRadius: "40px" }}
+              className="d-inline-block align-top"
+              alt="" /> </Navbar.Brand>
+            <Nav.Link href="#pricing"> <h5>{user.displayName}</h5> </Nav.Link>
+            <Button onClick={user.email ? handleLogOut : ""} type="submit">{user.email ? "Log out" : "sign in"}</Button>
+          </Nav>
+
+        </Navbar>
+
+
+
+
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <LoginPage></LoginPage>
+          </Route>
+          <PrivateRoute path="/route">
+            <ChooseRoute></ChooseRoute>
+          </PrivateRoute>
+        </Switch>
+      </Router>
     </UserContext.Provider>
   );
 }
--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -2,40 +2,86 @@ import React from 'react';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from "../../firebase.config"
-import { Button } from 'react-bootstrap';
+import { Button, Card, Form } from 'react-bootstrap';
 import { useContext } from 'react';
 import { UserContext } from '../../App';
 import { useHistory, useLocation } from 'react-router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import {faGoogle} from '@fortawesome/fontawesome-free-brands';
+import {Link} from 'react-router-dom'
+import { useState } from 'react';
 if (!firebase.apps.length) { firebase.initializeApp(firebaseConfig); }
 
 const LoginPage = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    const [user, setUser] = useContext(UserContext);
+    const [loggedUser, setLoggedUser] = useContext(UserContext);
+
+    const [logState, setLogState] = useState(false);
+
+    const [userInfo, setUserInfo] = useState({
+        name:"",
+        email:"",
+        password:""
+    })
 
     const history = useHistory();
     const location = useLocation();
     let { from } = location.state || { from: { pathname: "/" } };
-           
-     
+
+
     const handleGoogleSignIn = () => {
         firebase.auth()
             .signInWithPopup(provider)
             .then((result) => {
                 const user1 = result.user;
-                setUser(user1);
+                setLoggedUser(user1);
                 history.replace(from);
-                console.log(user);
+                console.log(loggedUser);
             }).catch((error) => {
                 const errorMessage = error.message;
                 console.log(errorMessage);
             });
 
     }
+    const handleOnChange = (event) =>{
+        console.log(event.target.name,event.target.value);
+       setUserInfo( {...userInfo,[event.target.name]: event.target.value});
+    }
+    const handleSignUpWithEmailAndPassword = () =>{
+      console.log(userInfo);
 
-    return (
-        <div>
-            <Button onClick={handleGoogleSignIn} className="info">Log In</Button>
-        </div>
+    }
+
+    return (<>
+        <Card className="mx-auto mt-5" style={{ width: '18rem' }}>
+        <Card.Body >
+            {
+                (!logState)?<>
+                    <Card.Title>Create an account</Card.Title>
+                    <input type="text" className="mt-2" placeholder="Enter your name"/>
+                    <input type="email"  className="mt-3" placeholder="Enter your email"/>
+                    <input type="password" className="mt-3" placeholder="Choose password"/>
+                    <input type="password"  className="mt-3" placeholder="Confirm password"/></>
+                    :
+                    <>
+                    <Card.Title>Sign in</Card.Title>
+                    <input type="email" name="email" onBlur={handleOnChange} className="mt-3" placeholder="Enter your email"/>
+                    <input type="password" name="password" onBlur={handleOnChange} className="mt-3" placeholder="Choose password"/>
+                    </>
+
+                    
+            }
+            
+               <Button type="submit" className="mt-4" onClick={handleSignUpWithEmailAndPassword} >{logState?"Sign in":"sign up"}</Button>
+               <Card.Text>{!logState?"Already have an account?":"Didn't have an account?"}<a onClick={()=>setLogState(!logState)}>{!logState?"Sign in":"sign up"}</a></Card.Text>
+            </Card.Body>
+        </Card>
+        <div className="d-flex justify-content-center mt-4">
+            <Button onClick={handleGoogleSignIn}> <FontAwesomeIcon className ='font-awesome me-2' icon={faGoogle} />Continue with Google</Button> 
+        </div>   
+        </>
     );
 };
 
